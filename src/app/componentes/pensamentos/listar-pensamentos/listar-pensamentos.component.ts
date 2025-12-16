a99ed813f07ccb5c3473e346fc3a563d7e2a6d85
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2025-12-16 16:46:51 -0300

    backend

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,3 +1,4 @@
+import { Pensamento } from './../pensamento';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -7,20 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
-  listaPensamentos = [
-    
-    {
-    conteudo: 'Passo informações para o componente filho',
-    autoria: 'componente pai',
-    modelo: 'modelo3'
-  },
-    {
-    conteudo: 'Minha propriedade é decorada com @Input()',
-    autoria: 'Componente filho',
-    modelo: 'modelo1'
-  }
-    
-  ]
+  listaPensamentos: Pensamento[] = [];
   constructor() { }
 
   ngOnInit(): void {
