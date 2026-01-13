ec23b8e6f766a318a77ccdfa0a0b33013a304b38
Lucas Caitaino Barreto <lucas.caitano.barreto@gmail.com>
2026-01-13 16:59:26 -0300

    parte 2

--- src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.spec.ts
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ExluirPensamentoComponent } from './excluir-pensamento.component';
+import { ExcluirPensamentoComponent } from './excluir-pensamento.component';
 
-describe('ExluirPensamentoComponent', () => {
-  let component: ExluirPensamentoComponent;
-  let fixture: ComponentFixture<ExluirPensamentoComponent>;
-
+describe('ExcluirPensamentoComponent', () => {
+  let component: ExcluirPensamentoComponent;
+  let fixture: ComponentFixture<ExcluirPensamentoComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExluirPensamentoComponent ]
+      declarations: [ ExcluirPensamentoComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ExluirPensamentoComponent);
+    fixture = TestBed.createComponent(ExcluirPensamentoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
